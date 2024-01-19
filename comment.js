f00194a660626d0fa1341a15ae98483ae57f8c97
Rémi JARA <82316285+icepick4@users.noreply.github.com>
2024-01-19 13:35:00 +0000

    Third copilot commit

---
+++ comment.js
@@ -0,0 +1,20 @@
+// Create web server
+
+// Import express
+const express = require('express');
+// Import body-parser
+const bodyParser = require('body-parser');
+// Import mongoose
+const mongoose = require('mongoose');
+// Import express-handlebars
+const exphbs = require('express-handlebars');
+// Import method-override
+const methodOverride = require('method-override');
+// Import path
+const path = require('path');
+// Import flash
+const flash = require('connect-flash');
+// Import session
+const session = require('express-session');
+// Import passport
+const passport = require('passport');
